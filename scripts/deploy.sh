b69d76628724b86b21aef8034445c31956b3c1ab
Justin Chase <justin.m.chase@gmail.com>
2019-08-12 01:23:48 -0500

    travis deploy debug

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+echo "Connecting to heroku..."
 heroku git:remote -a commit-karma
 heroku container:login
+
+echo "Publishing container..."
 heroku container:push web
 heroku container:release web
-
