ffeb320999a1a7c8f7a34b596158cc6406338db4
Justin Chase <justin.m.chase@gmail.com>
2019-08-12 02:04:59 -0500

    manual deploy is fine for now

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-
-echo "Connecting to heroku..."
-heroku git:remote -a commit-karma
 heroku container:login
-
-echo "Publishing container..."
 heroku container:push web
 heroku container:release web
