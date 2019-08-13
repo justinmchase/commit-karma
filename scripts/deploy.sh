72fe342cbbef29140de5d181e360efe6505a9a55
Justin Chase <justin.m.chase@gmail.com>
2019-08-12 21:49:18 -0500

    No idea why but travis is bombing out on deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# Install and Setup Heroku
+gem install heroku
 heroku container:login
+
+# Build
+npm ci
+npm run build
+
+# Create Docker Image and Push
 heroku container:push web
 heroku container:release web
