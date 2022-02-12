cca33f9f1e585220214c577564ac92f14690e57b
Justin Chase <justin.m.chase@gmail.com>
2022-02-12 17:01:26 -0600

    remove dynamic import

--- mod.ts
+++ mod.ts
@@ -1,7 +1,8 @@
+import { start } from "./src/mod.ts";
+
 if (import.meta.main) {
   try {
     console.log("starting...");
-    const { start } = await import("./src/mod.ts");
     await start();
   } catch (err) {
     console.log({ ...err });
