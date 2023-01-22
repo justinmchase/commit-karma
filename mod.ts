4871360a61658f77e0f33e519a00d49c638f15e1
Justin Chase <justin.m.chase@gmail.com>
2023-01-22 00:18:33 -0600

    main back to mod

---
+++ mod.ts
@@ -0,0 +1,12 @@
+import { start } from "./src/mod.ts";
+
+if (import.meta.main) {
+  try {
+    console.log("starting...");
+    await start();
+  } catch (err) {
+    console.log({ ...err });
+    console.log(err);
+  }
+  console.log("done.");
+}
